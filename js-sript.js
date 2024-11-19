4cc9e701ae64bd0ccc8852941bed5c49c30c436c
yami <qjasonc@gmail.com>
2024-11-19 00:50:33 -0800

    added RGBA

--- js-sript.js
+++ js-sript.js
@@ -1,6 +1,18 @@
 const container = document.querySelector("#container");
 const newGrid = document.querySelector("#new");
 
+//rgb function grabbed from stack overflow but I added the opacity
+function rgb(opacity)
+{
+    const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    let a = opacity;
+    const color = `rgba(${r},${g},${b},${a})`;
+    return color;
+}
+
 //Creates the 16x16 grid
 for(let i = 0; i < 16; i++)
 {
@@ -14,9 +26,14 @@ for(let i = 0; i < 16; i++)
         j.setAttribute("style", "flex: 1; border: 1px solid black;");
         i.appendChild(j);
 
+        //rgba
+        let opacity = 0.1;
+        const randomColor = rgb();
         //colors the squares when hovered
         j.addEventListener("mouseover", () => {
-            j.setAttribute("style", "flex: 1; background-color: black; border: 1px solid;");
+            j.setAttribute("style", "flex: 1; border: 1px solid;");
+            j.style.backgroundColor = rgb(opacity);//rgba
+            opacity += 0.1;
         });
         
     }
@@ -58,9 +75,15 @@ newGrid.addEventListener("click", () => {
                 const j = document.createElement("div");
                 j.setAttribute("style", "flex: 1; border: 1px solid black;");
                 i.appendChild(j);
-        
+                
+                //rgba
+                let opacity = 0.1;
+                const randomColor = rgb();
+                //colors the squares when hovered
                 j.addEventListener("mouseover", () => {
-                    j.setAttribute("style", "flex: 1; background-color: black; border: 1px solid;");
+                    j.setAttribute("style", "flex: 1; border: 1px solid;");
+                    j.style.backgroundColor = rgb(opacity);//rgba
+                    opacity += 0.1;
                 });
                 
             }
